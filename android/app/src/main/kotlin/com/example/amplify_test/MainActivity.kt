f21be6957e98697fde5c8f2cc99b4152bbfe8918
DrFill <adamfillion96@gmail.com>
2020-09-03 20:44:37 -0400

    Tutorial Part 1 Checkpoint

---
+++ android/app/src/main/kotlin/com/example/amplify_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.amplify_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:2bcfdafa-2769-43fe-958c-f57f53592f33",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_c94X8hvUB",
+                        "AppClientId": "5aiif8evt1f1dothtaabb4si03",
+                        "AppClientSecret": "12tfi0dafdasd9p23h37eltgia74ckg41oeqtvntohagr9k2i97",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
